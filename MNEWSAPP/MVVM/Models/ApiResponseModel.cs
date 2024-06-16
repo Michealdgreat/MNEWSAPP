5b925e87ef4086d6cd78492e2facf974e771c308
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-16 19:17:45 +0300

    Databinding models.

---
+++ MNEWSAPP/MVVM/Models/ApiResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNEWSAPP.MVVM.Models
+{
+    public class ApiResponseModel
+    {
+        public string? Status { get; set; }
+        public int TotalResults { get; set; }
+        public ObservableCollection<ArticleModel>? Articles { get; set; }
+    }
+}
---
+++ MNEWSAPP/MVVM/Models/ArticleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNEWSAPP.MVVM.Models
+{
+    public class ArticleModel
+    {
+        public SourceModel? Source { get; set; }
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public string? UrlToImage { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string? Content { get; set; }
+    }
+
+}
