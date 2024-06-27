3f2a9051ca09731a546e35bb1308c28853b51186
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-27 18:59:00 +0300

    Updated the Article Details View and CB file. Implemented Shell navigation and data persistence between views.

--- MNEWSAPP/MVVM/Views/ArticleDetailsView.xaml.cs
+++ MNEWSAPP/MVVM/Views/ArticleDetailsView.xaml.cs
@@ -2,11 +2,50 @@ using MNEWSAPP.MVVM.Models;
 
 namespace MNEWSAPP.MVVM.Views;
 
+[QueryProperty(nameof(Article), "article")]
 public partial class ArticleDetailsView : ContentPage
 {
-    public ArticleDetailsView(ArticleModel articleModel)
+    private ArticleModel? _article;
+
+    public ArticleModel? Article
+    {
+        get => _article;
+        set
+        {
+            _article = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ArticleDetailsView()
     {
         InitializeComponent();
-        BindingContext = articleModel;
+
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        BindingContext = _article;
+
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+        if (_article != null)
+        {
+            string? url = _article.Url;
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                var navigationParameter = new Dictionary<string, object>
+                {
+                    { "url", url }
+                };
+                await Shell.Current.GoToAsync($"{nameof(WebPageView)}", navigationParameter);
+            }
+        }
     }
 }
