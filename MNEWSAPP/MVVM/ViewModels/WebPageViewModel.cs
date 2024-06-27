ba34295b7e05ed084a48e17047d0005c4cd2b79c
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-27 19:03:40 +0300

    Created a WebView page to load articles from the source within the mnewsApp.

---
+++ MNEWSAPP/MVVM/ViewModels/WebPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace MNEWSAPP.MVVM.ViewModels
+{
+    public class WebPageViewModel : BindableObject
+    {
+        public ICommand BackCommand { get; }
+
+        public WebPageViewModel()
+        {
+            BackCommand = new Command(OnBackButtonPressed);
+        }
+
+        private async void OnBackButtonPressed()
+        {
+            await Shell.Current.GoToAsync("//IndexPage");
+        }
+    }
+}
