51ee2126a27274f31d5ec46a0e0a899ca484478a
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-27 19:07:30 +0300

    WebView page and CB file.

---
+++ MNEWSAPP/MVVM/Views/WebPageView.xaml.cs
@@ -0,0 +1,29 @@
+namespace MNEWSAPP.MVVM.Views;
+
+[QueryProperty(nameof(Url), "url")]
+public partial class WebPageView : ContentPage
+{
+    private string? _url;
+
+    public string? Url
+    {
+        get => _url;
+        set
+        {
+            _url = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public WebPageView()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        mnewsWebView.Source = Url;
+    }
+
+}
