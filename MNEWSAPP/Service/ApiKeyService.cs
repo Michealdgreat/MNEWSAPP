cab6ecf0ca99fad7d3dcb00d9342e901b08560a2
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-30 00:32:50 +0300

    API Key Setter and Getter Class

---
+++ MNEWSAPP/Service/ApiKeyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNEWSAPP.Service
+{
+    public class ApiKeyService
+    {
+        public ApiKeyService() { }
+
+
+        public async Task SetApiKey(string apiKey)
+        {
+            await SecureStorage.SetAsync("apiKey", apiKey);
+        }
+
+    }
+}
