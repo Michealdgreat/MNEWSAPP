dd0224e28b804bc2ebc6060cde58e53270116f0c
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-30 00:31:21 +0300

    Service class for Api Key Management.

--- MNEWSAPP/MVVM/Views/SetApiKeyView.xaml.cs
+++ MNEWSAPP/MVVM/Views/SetApiKeyView.xaml.cs
@@ -1,9 +1,32 @@
-namespace MNEWSAPP.MVVM.Views;
+using MNEWSAPP.Service;
 
-public partial class SetApiKeyView : ContentPage
+namespace MNEWSAPP.MVVM.Views
 {
-	public SetApiKeyView()
-	{
-		InitializeComponent();
-	}
+    public partial class SetApiKeyView : ContentPage
+    {
+        private readonly ApiKeyService apiKeyService;
+        public SetApiKeyView()
+        {
+            apiKeyService = new ApiKeyService();
+            InitializeComponent();
+        }
+
+        private async void SetApiKeyButton_Clicked(object sender, EventArgs e)
+        {
+
+            string apiKey = ApiKeyEntry.Text;
+
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                await DisplayAlert("Error", "Please enter a valid API key.", "OK");
+                return;
+            }
+            await apiKeyService.SetApiKey(apiKey);
+            await DisplayAlert("Success", "API key has been saved.", "OK");
+
+
+            await Shell.Current.GoToAsync("//IndexPage");
+        }
+    }
 }
