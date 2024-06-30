92c38b544a421539dce56081fb3d49262086d794
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-30 10:36:27 +0300

    Modified views and Set Data Binding.

--- MNEWSAPP/MVVM/Views/ExploreView.xaml.cs
+++ MNEWSAPP/MVVM/Views/ExploreView.xaml.cs
@@ -1,9 +1,47 @@
+using MNEWSAPP.MVVM.Models;
+using MNEWSAPP.Service;
+
 namespace MNEWSAPP.MVVM.Views;
 
 public partial class ExploreView : ContentPage
 {
-	public ExploreView()
+    private readonly GetNews _getNews;
+
+    public ExploreView(GetNews getNews)
 	{
 		InitializeComponent();
-	}
+        _getNews = getNews;
+    }
+
+    private async Task CategoryTapped(object sender, TappedEventArgs e, string category)
+    {
+        List<ArticleModel> articleModel = new();
+
+        var data = await _getNews.GetNewsAsync(category);
+        if (data != null)
+        {
+            foreach (var item in data)
+            {
+                articleModel.Add(item);
+            }
+        }
+
+        var navigationParameters = new Dictionary<string, object>
+            {
+                { "data", articleModel }
+            };
+
+        await Shell.Current.GoToAsync("CategoryView", navigationParameters);
+    }
+
+    private async void MIcrosoftTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Microsoft");
+    private async void AppleTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Apple");
+    private async void MetaTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Meta");
+    private async void AITapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "AI");
+    private async void VideosTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Videos");
+    private async void ENtertainmentTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Entertainment");
+    private async void TiktokTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Tiktok");
+    private async void SPortTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Sport");
+    private async void MondayTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Monday");
+    private async void BusinessTapped(object sender, TappedEventArgs e) => await CategoryTapped(sender, e, "Business");
 }
